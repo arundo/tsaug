ce5c72816aad9e2c0975a4ff70a590ca99caff90
tailaiw <29800495+tailaiw@users.noreply.github.com>
2020-04-03 15:44:36 -0500

    added quantize

--- src/tsaug/__init__.py
+++ src/tsaug/__init__.py
@@ -14,5 +14,6 @@ __version__ = "0.2"
 
 from ._augmenter.crop import Crop
 from ._augmenter.drift import Drift
+from ._augmenter.quantize import Quantize
 from ._augmenter.reverse import Reverse
 from ._augmenter.time_warp import TimeWarp
