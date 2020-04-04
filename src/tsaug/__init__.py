d5a2c34f3c5eda46ebffd132b305b8f0e2f4f4f2
tailaiw <29800495+tailaiw@users.noreply.github.com>
2020-04-03 21:49:24 -0500

    added convolve

--- src/tsaug/__init__.py
+++ src/tsaug/__init__.py
@@ -13,6 +13,7 @@ See https://arundo-tsaug.readthedocs-hosted.com complete documentation.
 __version__ = "0.2"
 
 from ._augmenter.add_noise import AddNoise
+from ._augmenter.convolve import Convolve
 from ._augmenter.crop import Crop
 from ._augmenter.drift import Drift
 from ._augmenter.quantize import Quantize
