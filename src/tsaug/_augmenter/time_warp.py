7d9168e259ee130251b8ea666308e07b60540d58
tailaiw <29800495+tailaiw@users.noreply.github.com>
2020-04-06 14:39:45 -0500

    better time warping method

--- src/tsaug/_augmenter/time_warp.py
+++ src/tsaug/_augmenter/time_warp.py
@@ -5,8 +5,16 @@ from .base import _Augmenter
 
 
 class TimeWarp(_Augmenter):
-    def __init__(self, n_speed_change=3, repeats=1, prob=1.0, seed=None):
+    def __init__(
+        self,
+        n_speed_change=3,
+        maxmin_speed_ratio=3,
+        repeats=1,
+        prob=1.0,
+        seed=None,
+    ):
         self.n_speed_change = n_speed_change
+        self.maxmin_speed_ratio = maxmin_speed_ratio
         super().__init__(repeats=repeats, prob=prob, seed=seed)
 
     @classmethod
@@ -29,6 +37,43 @@ class TimeWarp(_Augmenter):
             )
         self._n_speed_change = n
 
+    @property
+    def maxmin_speed_ratio(self):
+        return self._maxmin_speed_ratio
+
+    @maxmin_speed_ratio.setter
+    def maxmin_speed_ratio(self, n):
+        MAXMIN_SPEED_RATIO_ERROR_MSG = (
+            "Parameter `maxmin_speed_ratio` must be a number greater or equal "
+            " to 1.0, "
+            "a 2-tuple of such numbers representing an interval, "
+            "or a list of such numbers."
+        )
+        if not isinstance(n, (float, int)):
+            if isinstance(n, list):
+                if len(n) == 0:
+                    raise ValueError(MAXMIN_SPEED_RATIO_ERROR_MSG)
+                if not all([isinstance(nn, (float, int)) for nn in n]):
+                    raise TypeError(MAXMIN_SPEED_RATIO_ERROR_MSG)
+                if not all([nn >= 0 for nn in n]):
+                    raise ValueError(MAXMIN_SPEED_RATIO_ERROR_MSG)
+            elif isinstance(n, tuple):
+                if len(n) != 2:
+                    raise ValueError(MAXMIN_SPEED_RATIO_ERROR_MSG)
+                if (not isinstance(n[0], (float, int))) or (
+                    not isinstance(n[1], (float, int))
+                ):
+                    raise TypeError(MAXMIN_SPEED_RATIO_ERROR_MSG)
+                if n[0] > n[1]:
+                    raise ValueError(MAXMIN_SPEED_RATIO_ERROR_MSG)
+                if (n[0] < 0) or (n[1] < 0):
+                    raise ValueError(MAXMIN_SPEED_RATIO_ERROR_MSG)
+            else:
+                raise TypeError(MAXMIN_SPEED_RATIO_ERROR_MSG)
+        elif n < 0:
+            raise ValueError(MAXMIN_SPEED_RATIO_ERROR_MSG)
+        self._maxmin_speed_ratio = n
+
     def _augment_core(self, X, Y):
         rand = np.random.RandomState(self.seed)
         N, T, C = X.shape
@@ -36,31 +81,34 @@ class TimeWarp(_Augmenter):
             L = Y.shape[2]  # type: int
 
         anchors = np.arange(
-            0.5 / self.n_speed_change, 1, 1 / self.n_speed_change
-        )  # type: np.ndarray
-        R = (
-            rand.uniform(
-                low=-0.5 / self.n_speed_change,
-                high=0.5 / self.n_speed_change,
-                size=(N, self.n_speed_change),
+            0,
+            1 + 1 / (self.n_speed_change + 1) / 2,
+            1 / (self.n_speed_change + 1),
+        ) * (T - 1)
+
+        if isinstance(self.maxmin_speed_ratio, (float, int)):
+            maxmin_speed_ratio = np.ones(N) * self.maxmin_speed_ratio
+        elif isinstance(self.maxmin_speed_ratio, tuple):
+            maxmin_speed_ratio = rand.uniform(
+                low=self.maxmin_speed_ratio[0],
+                high=self.maxmin_speed_ratio[1],
+                size=N,
             )
-            * 0.5
-        )  # type: np.ndarray
-        anchor_values = R + np.arange(
-            0.5 / self.n_speed_change, 1, 1 / self.n_speed_change
-        )  # type: np.ndarray
-
-        anchors = np.append([0], anchors)
-        anchors = np.append(anchors, [1])
-        anchors = anchors * (T - 1)
-
+        else:
+            maxmin_speed_ratio = rand.choice(self.maxmin_speed_ratio, size=N)
+        anchor_values = rand.uniform(
+            low=1 / maxmin_speed_ratio.reshape(N, 1),
+            high=1.0,
+            size=(N, self.n_speed_change + 1),
+        )
+        anchor_values = (
+            anchor_values.cumsum(axis=1)
+            / anchor_values.sum(axis=1, keepdims=True)
+            * (T - 1)
+        )
         anchor_values = np.concatenate(
             [np.zeros((N, 1)), anchor_values], axis=1
         )
-        anchor_values = np.concatenate(
-            [anchor_values, np.ones((N, 1))], axis=1
-        )
-        anchor_values = anchor_values * (T - 1)
 
         warp = PchipInterpolator(x=anchors, y=anchor_values, axis=1)(
             np.arange(T)
