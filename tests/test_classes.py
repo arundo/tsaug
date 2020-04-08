5657763129ffc451ee71a9636f110c37ae2e2135
tailaiw <29800495+tailaiw@users.noreply.github.com>
2020-04-08 09:59:08 -0500

    test

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -14,19 +14,6 @@ from tsaug import (
     TimeWarp,
 )
 
-augmenters = [
-    AddNoise(),
-    Convolve(),
-    Crop(size=100),
-    Drift(),
-    Dropout(),
-    Pool(),
-    Quantize(),
-    Resize(size=100),
-    Reverse(),
-    TimeWarp(),
-]
-
 N = 10
 T = 100
 C = 3
@@ -41,6 +28,30 @@ Y1 = np.random.choice(2, size=T).astype(int)
 Y2 = np.random.choice(2, size=(N, T)).astype(int)
 Y3 = np.random.choice(2, size=(N, T, L)).astype(int)
 
+augmenters = [
+    AddNoise(repeats=M, prob=0.5),
+    AddNoise(loc=(-1.0, 1.0), scale=(0.1, 0.2)),
+    AddNoise(loc=[-1.0, 1.0], scale=[0.1, 0.2]),
+    AddNoise(distr="laplace"),
+    AddNoise(distr="uniform"),
+    AddNoise(kind="multiplicative"),
+    AddNoise(per_channel=False, normalize=False),
+    Convolve(repeats=M, prob=0.5),
+    Convolve(window=["hann", "blackman"]),
+    Convolve(window=("gaussian", 1)),
+    Convolve(size=(7, 11)),
+    Convolve(size=[7, 11]),
+    Convolve(per_channel=True),
+    Crop(size=100),
+    Drift(),
+    Dropout(),
+    Pool(),
+    Quantize(),
+    Resize(size=100),
+    Reverse(),
+    TimeWarp(),
+]
+
 
 @pytest.mark.parametrize("augmenter", augmenters)
 def test_X1_Y0(augmenter):
@@ -48,7 +59,10 @@ def test_X1_Y0(augmenter):
     1D X, no Y
     """
     X_aug = augmenter.augment(X1)
-    assert X_aug.shape == (T,)
+    if augmenter.repeats == 1:
+        assert X_aug.shape == (T,)
+    else:
+        assert X_aug.shape == (M, T)
 
     # check X_aug is not a view of X
     Xc = X1.copy()
@@ -62,8 +76,12 @@ def test_X1_Y1(augmenter):
     1D X, 1D Y
     """
     X_aug, Y_aug = augmenter.augment(X1, Y1)
-    assert X_aug.shape == (T,)
-    assert Y_aug.shape == (T,)
+    if augmenter.repeats == 1:
+        assert X_aug.shape == (T,)
+        assert Y_aug.shape == (T,)
+    else:
+        assert X_aug.shape == (M, T)
+        assert Y_aug.shape == (M, T)
 
     # check X_aug is not a view of X
     Xc = X1.copy()
@@ -82,7 +100,7 @@ def test_X2_Y0(augmenter):
     2D X, no Y
     """
     X_aug = augmenter.augment(X2)
-    assert X_aug.shape == (N, T)
+    assert X_aug.shape == (N * augmenter.repeats, T)
 
     # check X_aug is not a view of X
     Xc = X2.copy()
@@ -96,8 +114,8 @@ def test_X2_Y2(augmenter):
     2D X, 2D Y
     """
     X_aug, Y_aug = augmenter.augment(X2, Y2)
-    assert X_aug.shape == (N, T)
-    assert Y_aug.shape == (N, T)
+    assert X_aug.shape == (N * augmenter.repeats, T)
+    assert Y_aug.shape == (N * augmenter.repeats, T)
 
     # check X_aug is not a view of X
     Xc = X2.copy()
@@ -116,7 +134,7 @@ def test_X3_Y0(augmenter):
     3D X, no Y
     """
     X_aug = augmenter.augment(X3)
-    assert X_aug.shape == (N, T, C)
+    assert X_aug.shape == (N * augmenter.repeats, T, C)
 
     # check X_aug is not a view of X
     Xc = X3.copy()
@@ -130,8 +148,8 @@ def test_X3_Y2(augmenter):
     3D X, 2D Y
     """
     X_aug, Y_aug = augmenter.augment(X3, Y2)
-    assert X_aug.shape == (N, T, C)
-    assert Y_aug.shape == (N, T)
+    assert X_aug.shape == (N * augmenter.repeats, T, C)
+    assert Y_aug.shape == (N * augmenter.repeats, T)
 
     # check X_aug is not a view of X
     Xc = X3.copy()
@@ -150,8 +168,8 @@ def test_X3_Y3(augmenter):
     3D X, 3D Y
     """
     X_aug, Y_aug = augmenter.augment(X3, Y3)
-    assert X_aug.shape == (N, T, C)
-    assert Y_aug.shape == (N, T, L)
+    assert X_aug.shape == (N * augmenter.repeats, T, C)
+    assert Y_aug.shape == (N * augmenter.repeats, T, L)
 
     # check X_aug is not a view of X
     Xc = X3.copy()
